91e4241946706fbd8bbd223248e90b51fd8b783f
Evgeny Lahav <Evgeny_Lahav@amat.com>
2019-08-31 01:04:18 +0300

    first commit of bloggy

---
+++ src/build/precache-manifest.ffc97109ebdbfac1647d4f9daa30ce9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "69d994bff7fe5ca601ed",
+    "url": "/static/js/main.21a11025.chunk.js"
+  },
+  {
+    "revision": "640b69ba3cfeb2004209",
+    "url": "/static/js/2.6b912c02.chunk.js"
+  },
+  {
+    "revision": "69d994bff7fe5ca601ed",
+    "url": "/static/css/main.a4f54d6f.chunk.css"
+  },
+  {
+    "revision": "23676b99696d3a0dc9e86b6575c2e4dd",
+    "url": "/index.html"
+  }
+];
